5ba369928539bac00e30bd00455122f665a84aa3
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2026-09-22 12:12:46 +0900

    font調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,26 +10,12 @@ export default defineConfig({
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
+			provider: fontProviders.google(),
+			name: 'Noto Sans JP',
+			cssVariable: '--font-noto-sans-jp',
 			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
+			weights: ['400', '700'],
+			styles: ['normal'],
 		},
 	],
 	server: {
